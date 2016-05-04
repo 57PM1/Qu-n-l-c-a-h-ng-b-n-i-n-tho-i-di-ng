a85177626403700d3bce11dc4907b3f8538246d7
ngotrite <ngotrite@gmail.com>
2016-05-04 08:59:38 +0700

    form quản lý tài khoản
    Đã cài đặt BUS, DAO, DTO cho quản lý nhà cung cấp, quản lý nhà sản xuất,
    quản lý tài khoản

---
+++ DAO/DataProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAO
+{
+    public class DataProvider
+    {
+        //Kết nối 
+        public static SqlConnection KetNoi()
+        {
+            string sChuoiKetNoi = @"Data Source=TENT\SQLEXPRESS;Initial Catalog=QL_BanHangDienThoai;Persist Security Info=True;User ID=sa;Password=1";
+            SqlConnection conn = new SqlConnection(sChuoiKetNoi);
+            conn.Open();
+            return conn;
+        }
+        //Đóng kết nối
+        public static void DongKetNoi(SqlConnection conn)
+        {
+            conn.Close();
+        }
+        //Lấy DataTable
+        public static DataTable LayDataTable(string sTruyVan, SqlConnection conn)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(sTruyVan, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        //Thực thi truy vấn ExcuteNonQuery
+        public static bool ThucThiTruyVanNonQuery(string sTruyVan, SqlConnection conn)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sTruyVan, conn);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ DTO/HDN_DTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HDN_DTO
+    {
+        public int MaHDN { get; set; }
+        public int MaNV { get; set; }
+        public int MaNhaCC { get; set; }
+        public string NgayNhap { get; set; }
+    }
+}
